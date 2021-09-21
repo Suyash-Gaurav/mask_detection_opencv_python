d9ed12079549586d1a2d16fc5efe7f0c54b3e51c
suyash gaurav <83945946+Suyash-Gaurav@users.noreply.github.com>
2021-09-21 11:46:27 +0545

    Add files via upload

--- DataCollector.py
+++ DataCollector.py
@@ -1,20 +1,21 @@
 import cv2
-video =cv2.VideoCapture(0)
-facedetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+video = cv2.VideoCapture(0)
+facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+count=0
 while True:
     sucess, vid = video.read()
-    face=facedetect.detectMultiScale(vid,1.5,5)
+    face = facedetect.detectMultiScale(vid, 1.8, 5)
 
-    for x,y,w,z in face:
-        cv2.rectangle(vid, (x, y), (x + w, +z), (0, 255, 0), 3)
-        cv2.imshow("vid", vid)
-
-    k =  cv2.waitKey(1)
-    if k == ord("a"):
+    for x, y, w, h in face:
+        count+=1
+        name='./images/mask/' + str(count) +".jpg"
+        print ('creating images.............'+ name)
+        cv2.imwrite(name,vid[y:y+h,x:x+w])
+        cv2.rectangle(vid, (x, y), (x + w, y+h), (0, 255, 0), 3)
+    cv2.imshow("vid", vid)
+    k = cv2.waitKey(1)
+    if count ==500 :
         break
-
-
 
 
 
